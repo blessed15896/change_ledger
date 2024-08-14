7ce9cf1d7f44520c60f01d0aa1048d524399e3a1
Blessed Sibanda <blessed15896@gmail.com>
2024-08-14 07:52:03 +0200

    Create gem using bundler

---
+++ lib/change_ledger.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "change_ledger/version"
+
+module ChangeLedger
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/change_ledger_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ChangeLedger do
+  it "has a version number" do
+    expect(ChangeLedger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
